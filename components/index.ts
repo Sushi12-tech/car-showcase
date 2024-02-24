ed2d760aa94a629c2d1c07946a05febd4cedf654
Sushant Sagar <sushi.sagar21@gmail.com>
2024-02-24 19:55:21 +0530

    final-build

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import SearchBar from './Searchbar';
+import CustomFilter from './CustomFilter';
+export{
+    CustomButton,
+    Hero,
+    Footer,
+    NavBar,
+    SearchBar,
+    CustomFilter
+}
